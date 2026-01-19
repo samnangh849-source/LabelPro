3e884f6c988adf2d6e178d9df6a34664309a9189
samnangh849-source <samnangh849@gmail.com>
2026-01-20 02:37:43 +0700

    Fix: Final import path and prop definition corrections in FlexiLabel.tsx

--- components/FlexiLabel.tsx
+++ components/FlexiLabel.tsx
@@ -1,15 +1,17 @@
 
 import React from 'react';
-import { LabelData } from '../types.ts';
-import { SmartText, SmartQR } from './SmartElements.tsx';
+import { LabelData } from '../types';
+import { SmartText, SmartQR } from './SmartElements';
 
 interface FlexiLabelProps {
   data: LabelData;
   qrValue: string;
   isDesignMode: boolean;
+  lineLeft?: number;
+  lineRight?: number;
 }
 
-const FlexiLabel: React.FC<FlexiLabelProps> = ({ data, qrValue, isDesignMode }) => {
+const FlexiLabel: React.FC<FlexiLabelProps> = ({ data, qrValue, isDesignMode, lineRight = 2.5 }) => {
   const totalAmount = parseFloat(data.total);
   const paymentLower = data.payment.toLowerCase();
   
@@ -28,7 +30,10 @@ const FlexiLabel: React.FC<FlexiLabelProps> = ({ data, qrValue, isDesignMode }) 
   const combinedLocationAddress = `${data.location}, ${data.address}`;
 
   return (
-    <div className="flex flex-col w-[60mm] h-[80mm] bg-white text-black font-sans overflow-hidden relative border-[2.5px] border-black box-border p-0">
+    <div 
+        className="flex flex-col w-[60mm] h-[80mm] bg-white text-black font-sans overflow-hidden relative border-black box-border p-0"
+        style={{ borderWidth: `${lineRight}px` }}
+    >
         {/* Left Safety Margin Accent */}
         <div className="absolute left-0 top-0 bottom-0 w-1.5 bg-black z-20"></div>
 
