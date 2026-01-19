3adda1fa12b3ffa6021022d5b1dc7a99d30804b6
samnangh849-source <samnangh849@gmail.com>
2026-01-20 00:57:37 +0700

    T1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // សំខាន់បំផុតសម្រាប់ GitHub Pages
   server: {
     port: 3000,
   },
